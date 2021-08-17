bc56564d0cca354fdeab9d0d3b3392f0aba07236
Nechaev S <{ID}+{username}@users.noreply.github.com>
2021-08-17 15:38:26 +0300

    Initial Commit

---
+++ TwoView2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoView2
+//
+//  Created by Nechaev Sergey  on 17.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
